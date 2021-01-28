294c221576c597cf17785563ae71b6c9fe13b280
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-01-28 19:44:06 +0100

    neuter the idiotic gmail confidential crap
    the enable right click extension wasn't enough, but now I can copy
    whatever I want.

--- script.js
+++ script.js
@@ -350,6 +350,9 @@ setGetSet(window, 'addEventListener', () => function(type, listener, options) {
             console.log('denied listener before unload', listener);
             return;
         }
+        if (!listener) {
+            return;
+        }
 
         // TODO: just strip out the debugger statement
         if (debuggerRx.test(listener.toString())) {
@@ -367,6 +370,9 @@ setGetSet(window, 'setInterval', () => function(func, time) {
         if (debuggerRx.test(func.toString())) {
             console.log("Debugger in function, refusing");
             //console.log(listener); don't print it, it could sniff in its toString()
+            return;
+        }
+        if (!func) {
             return;
         }
         if (func.toString().indexOf('devtools') !== -1) { // lol just break the skiddies
@@ -954,6 +960,24 @@ if (window.location.hostname.indexOf("slack.com") != -1) { // lgtm [js/incomplet
         }
     });
     observer.observe(document, {childList: true, subtree: true});
+}
+
+// Neuter the dumb confidential shit, I like to select the text I read
+if (window.location.hostname.indexOf("mail.google.com") != -1) { // lgtm [js/incomplete-url-substring-sanitization]
+    setGetSet(window, 'getSelection', () => function() { console.log("tried window getselection"); }, () => function() { });
+    setGetSet(document, 'getSelection', () => function() { console.log("tried document getselection"); }, () => function() { });
+
+    // Just in case they try more in the future, we can try this
+    //delete window.selection;
+    //setGetSet(window, 'onselectionchange', () => function() { console.log("onselectionchange"); }, () => function() {});
+    //setGetSet(document, 'onselectstart', () => function() { console.log("tried onselectstart"); }, () => function() {});
+    //setGetSet(document, 'createRange', () => function() { console.log("tried to create selection range"); }, () => function() { });
+
+    setTimeout(function() {
+        addStyleString(".div { user-select: auto !important; }");
+        addStyleString(".p { user-select: auto !important; }");
+        addStyleString(".an { user-select: auto !important; }");
+    }, 100);
 }
 
 ////////////////////////////////////////////
